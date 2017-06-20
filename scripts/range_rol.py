b092d72c934496db80def3914636eafd268ec670
Ricardo Quesada <ricardoquesada@gmail.com>
2017-06-20 07:38:20 -0700

    range_rol: don't print "normal" scroll values

--- scripts/range_rol.py
+++ scripts/range_rol.py
@@ -9,12 +9,12 @@ for YY in range(8):
         addr_total = addr_base + addr_rel
         print "addr base: $%04x - addr rel: $%04x = $%04x" % (addr_base, addr_rel, addr_total)
 
-print "Normal *********"
-BITMAP_ADDR = 0
-for YY in range(8):
-    print "-"
-    for SS in range(40):
-        addr_base = BITMAP_ADDR + (39 - SS) * 8
-        addr_rel = YY
-        addr_total = addr_base + addr_rel
-        print "addr base: $%04x - addr rel: $%04x = $%04x" % (addr_base, addr_rel, addr_total)
+#print "Normal *********"
+#BITMAP_ADDR = 0
+#for YY in range(8):
+#    print "-"
+#    for SS in range(40):
+#        addr_base = BITMAP_ADDR + (39 - SS) * 8
+#        addr_rel = YY
+#        addr_total = addr_base + addr_rel
+#        print "addr base: $%04x - addr rel: $%04x = $%04x" % (addr_base, addr_rel, addr_total)
