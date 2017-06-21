d4a343730d2c9ee4b41229076e610420a8fb6007
Ricardo Quesada <ricardoquesada@gmail.com>
2017-06-20 23:03:03 -0700

    using code generator

--- intro.s
+++ intro.s
@@ -3,7 +3,7 @@
 ;
 ; Original code by Riq
 ; https://github.com/ricardoquesada/c64-misc
-; 
+;
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 
 
@@ -24,11 +24,15 @@ BITMAP_ADDR = $6000 + 8 * 40 * 12
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; ZP variables
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
-ZP_SYNC_RASTER = $fe                    ; used to sync raster
+ZP_SYNC_RASTER = $b0                    ; used to sync raster
+ZP_BIT_IDX_TOP = $b1                    ; bit to be displayed
+ZP_BIT_IDX_BOTTOM = $b2
 
 
 .segment "CODE"
         sei
+
+        jsr generator
 
         lda #$35                        ; no basic, no kernal
         sta $01
@@ -47,10 +51,14 @@ ZP_SYNC_RASTER = $fe                    ; used to sync raster
         lda #0
         sta $d021                       ; background color
         sta ZP_SYNC_RASTER
+        sta ZP_BIT_IDX_BOTTOM
+
+        lda #1
+        sta ZP_BIT_IDX_TOP
 
         lda #%00001000                  ; no scroll, hires (mono color), 40-cols
         sta $d016
-        
+
         lda #%00111011                  ; bitmap mode, default scroll-Y position, 25-rows
         sta $d011
 
@@ -63,7 +71,7 @@ ZP_SYNC_RASTER = $fe                    ; used to sync raster
 
         lda #01                         ; enable raster irq
         sta $d01a
-      
+
         lda #$50
         sta $d012
 
@@ -71,7 +79,7 @@ ZP_SYNC_RASTER = $fe                    ; used to sync raster
         ldy #>irq
         stx $fffe
         sty $ffff
-  
+
         lda $dc0d                       ; ack possible interrupts
         lda $dd0d
         asl $d019
@@ -113,9 +121,9 @@ load_scroll_addr = * + 1
         cmp #$ff
         bne next
         ldx #1
-        stx bit_idx_top
+        stx ZP_BIT_IDX_TOP
         ldx #0
-        stx bit_idx_bottom
+        stx ZP_BIT_IDX_BOTTOM
         ldx #<scroll_text
         ldy #>scroll_text
         stx load_scroll_addr
@@ -143,18 +151,20 @@ next:
         ; scroll top 8 bytes
         ; YY = char rows
         ; SS = bitmap cols
-        .repeat 8, YY
-                lda ($fc),y
-                ldx bit_idx_top         ; set C according to the current bit index
-:               asl
-                dex
-                bpl :-
+;        .repeat 8, YY
+;                lda ($fc),y
+;                ldx ZP_BIT_IDX_TOP         ; set C according to the current bit index
+;:               asl
+;                dex
+;                bpl :-
+;
+;                .repeat 40, SS
+;                        rol BITMAP_ADDR + (39 - SS) * 8 + (40*8) * ((SS+YY) / 8) + (SS+YY) .MOD 8
+;                .endrepeat
+;                iny                     ; byte of the char
+;        .endrepeat
 
-                .repeat 40, SS
-                        rol BITMAP_ADDR + (39 - SS) * 8 + (40*8) * ((SS+YY) / 8) + (SS+YY) .MOD 8
-                .endrepeat
-                iny                     ; byte of the char
-        .endrepeat
+        jsr $c000
 
 
         ; fetch bottom part of the char
@@ -166,10 +176,10 @@ next:
         adc #04                         ; the same thing as adding 1024
         sta $fd
 
-        lda bit_idx_bottom              ; if bit_idx_bottom == 0, then char + 1
+        lda ZP_BIT_IDX_BOTTOM              ; if bit_idx_bottom == 0, then char + 1
         cmp #$07
         bne :+
-        cld
+        clc
         lda $fc
         adc #$01
         sta $fc
@@ -183,21 +193,22 @@ next:
         ; scroll middle 8 bytes
         ; YY = char rows
         ; SS = bitmap cols
-        .repeat 8, YY
-                lda ($fc),y
-                ldx bit_idx_bottom      ; set C according to the current bit index
-:               asl
-                dex
-                bpl :-
+;        .repeat 8, YY
+;                lda ($fc),y
+;                ldx ZP_BIT_IDX_BOTTOM      ; set C according to the current bit index
+;:               asl
+;                dex
+;                bpl :-
+;
+;                .repeat 40, SS
+;                        rol BITMAP_ADDR + 40 * 8 + (39 - SS) * 8 + (40*8) * ((SS+YY) / 8) + (SS+YY) .MOD 8
+;                .endrepeat
+;                iny                     ; byte of the char
+;        .endrepeat
 
-                .repeat 40, SS
-                        rol BITMAP_ADDR + 40 * 8 + (39 - SS) * 8 + (40*8) * ((SS+YY) / 8) + (SS+YY) .MOD 8
-                .endrepeat
-                iny                     ; byte of the char
-        .endrepeat
+        jsr $c400
 
-
-        ldx bit_idx_top                 ; bit idx top
+        ldx ZP_BIT_IDX_TOP                 ; bit idx top
         inx
         cpx #8
         bne l1
@@ -210,7 +221,7 @@ next:
         bcc l1
         inc load_scroll_addr+1
 l1:
-        stx bit_idx_top
+        stx ZP_BIT_IDX_TOP
 
 
         dex                             ; bit_idx_bottom = bit_idx_top - 1
@@ -219,14 +230,10 @@ l1:
 
         ldx #7
 l2:
-        stx bit_idx_bottom
+        stx ZP_BIT_IDX_BOTTOM
 
         rts
 
-bit_idx_top:
-        .byte 1                         ; points to the bit displayed
-bit_idx_bottom:
-        .byte 0                         ; points to the bit displayed
 .endproc
 
 
@@ -255,10 +262,197 @@ scroll_text:
         scrcode "  "
         .byte $ff
 
+.proc generator
+        ldx #0
+        lda #$2e                        ; set everything with 'rol'
+l0:
+        sta $c000,x
+        sta $c100,x
+        sta $c200,x
+        sta $c300,x
+        sta $c400,x
+        sta $c500,x
+        sta $c600,x
+        sta $c700,x
+        sta $c800,x
+        dex
+        bne l0
+
+        ldx #<($c000)                   ; where to generate the code
+        ldy #>($c000)
+        stx $92
+        sty $93
+
+        ldy #0
+        sty $81                         ; index to table_rel
+
+        jsr generate_loop
+
+        ldx #<($c400)                   ; where to generate the code
+        ldy #>($c400)
+        stx $92
+        sty $93
+
+        ldy #8
+        sty $81                         ; index to table_rel
+
+        jmp generate_loop
+.endproc
+
+.proc generate_loop
+
+        lda #8                          ; repeat 8 times
+        sta $80
+
+l1_1:
+        jsr generate_jsr                ; jsr loop_jump
+
+        ldy $81
+        ldx #0
+l1:
+
+        clc
+        lda table_base_lo,x             ; base always uses x
+        adc table_rel_lo,y              ; rel always uses y since y will vary in each iteration
+        sta $90
+        lda table_base_hi,x
+        adc table_rel_hi,y
+        sta $91
+
+        jsr generate_rol_addr
+
+        iny
+        inx
+        cpx #40
+        bne l1
+
+        jsr generate_iny
+
+        inc $81                         ; Y counter. gets incremented once per loop. offset to rel. addresses
+        dec $80                         ; repeat 8 times (once per bit)
+        bne l1_1
+
+        jmp generate_rts
+.endproc
+
+.proc generate_jsr
+        lda #$20                        ; 'jsr' opcode
+        ldy #0
+        sta ($92),y
+
+        lda #<loop_jump
+        iny
+        sta ($92),y
+
+        lda #>loop_jump
+        iny
+        sta ($92),y
+
+        jsr ptr_plus_3
+        jmp ptr_plus_1
+.endproc
+
+.proc generate_iny
+        jsr ptr_minus_1
+        lda #$c8                       ; 'iny' opcode
+        ldy #0
+        sta ($92),y
+        jmp ptr_plus_1
+.endproc
+
+.proc generate_rts
+        jsr ptr_minus_1
+        lda #$60                        ; 'rts' opcode
+        ldy #0
+        sta ($92),y
+        jmp ptr_plus_1
+.endproc
+
+
+.proc generate_rol_addr
+        tya
+        pha
+
+        ldy #$0
+        lda $90                        ; lo addr to save
+        sta ($92),y
+        iny
+        lda $91                        ; hi addr to save
+        sta ($92),y
+
+        jsr ptr_plus_3
+        pla
+        tay
+        rts
+.endproc
+
+.proc ptr_plus_3
+        clc
+        lda $92
+        adc #$03
+        sta $92
+        lda $93
+        adc #$00
+        sta $93
+        rts
+.endproc
+
+.proc ptr_plus_1
+        clc
+        inc $92
+        bcc end
+        inc $93
+end:
+        rts
+.endproc
+
+.proc ptr_minus_1
+        sec
+        dec $92
+        bcs end
+        dec $93
+end:
+        rts
+.endproc
+
+.proc loop_jump
+        lda ($fc),y
+        ldx ZP_BIT_IDX_BOTTOM      ; set C according to the current bit index
+:       asl
+        dex
+        bpl :-
+        rts
+.endproc
+
+
+PIXEL_BASE = $6f00
+
+table_base_lo:
+        .repeat 40, XX
+                .byte <(PIXEL_BASE + $0138 - 8 * XX)
+        .endrepeat
+table_base_hi:
+        .repeat 40, XX
+                .byte >(PIXEL_BASE + $0138 - 8 * XX)
+        .endrepeat
+
+table_rel_lo:
+        .repeat 7, YY
+                .repeat 8, XX
+                        .byte <(XX + $0140 * YY)
+                .endrepeat
+        .endrepeat
+
+table_rel_hi:
+        .repeat 7, YY
+                .repeat 8, XX
+                        .byte >(XX + $0140 * YY)
+                .endrepeat
+        .endrepeat
 
 ; charset to be used for sprites here
 charset:
-;        .incbin "res/arleka_font_caren_remix0A-charset.bin" 
+;        .incbin "res/arleka_font_caren_remix0A-charset.bin"
         .incbin "res/arleka_font_caren_remix0A_invert-charset.bin"
 
 .segment "BMP_VS"
--- scripts/range_rol.py
+++ scripts/range_rol.py
@@ -1,12 +1,24 @@
 
-print "Inclinado **********"
-BITMAP_ADDR = 0
+BITMAP_ADDR = 0x6000 + 8 * 40 * 12
+
+print "Inclinado Top **********"
 for YY in range(8):
     print "-"
     last_value = 0
     for SS in range(40):
         addr_base = BITMAP_ADDR + (39 - SS) * 8
         addr_rel = (40*8) * ((SS+YY) / 8) + (SS+YY) % 8
+        addr_total = addr_base + addr_rel
+        print "addr base: $%04x + addr rel: $%04x = $%04x (diff=$%04x)" % (addr_base, addr_rel, addr_total, addr_total - last_value)
+        last_value = addr_total
+
+print "Inclinado Bottom **********"
+for YY in range(8):
+    print "-"
+    last_value = 0
+    for SS in range(40):
+        addr_base = BITMAP_ADDR + (39 - SS) * 8
+        addr_rel = 40 * 8 + (40*8) * ((SS+YY) / 8) + (SS+YY) % 8
         addr_total = addr_base + addr_rel
         print "addr base: $%04x + addr rel: $%04x = $%04x (diff=$%04x)" % (addr_base, addr_rel, addr_total, addr_total - last_value)
         last_value = addr_total
